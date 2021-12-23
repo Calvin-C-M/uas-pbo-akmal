15cf9c9457f6aa2daa277c666eabddcfad6bdba3
Calvin-C-M <montolalu28@gmail.com>
2021-12-23 08:27:56 +0700

    Gak jadi nomor 1 + tambahin deskripsi di main

--- UASSoal2/src/main/java/View/Main.java
+++ UASSoal2/src/main/java/View/Main.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Nama Project         : UAS Soal 2
+ * Nama                 : Calvin Calfi Montolalu
+ * NPM                  : 140810200053
+ * Kelas                : A
+ * Tanggal Buat         : 23/12/2021
+ * Deskripri Project    :
  */
 package View;
 
